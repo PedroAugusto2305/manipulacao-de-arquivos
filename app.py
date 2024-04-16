cb9ca1d4a28a63feaa61a65e32fe6dff2e033218
PedroAugusto2305 <p.augusto_23@outlook.com>
2024-04-16 15:42:48 -0300

    Gerador de IDs

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
 from models.funcionario import Funcionario
 
+def carregar_ultimo_id():
+    try:
+        with open('./arquivos/ultimo_id.txt', 'r', encoding='utf-8') as arquivo:
+            conteudo = arquivo.read().strip()
+            ultimo_id = int(conteudo) if conteudo else 0
+    except FileNotFoundError:
+        ultimo_id = 0
+    return ultimo_id
+
+def salvar_id(ultimo_id):
+    with open('./arquivos/ultimo_id.txt', 'w', encoding='utf-8') as arquivo:
+        arquivo.write(str(ultimo_id))       
+
 # listar todos funcionários
 
 def listar_funcionarios():
@@ -9,10 +22,14 @@ def listar_funcionarios():
 
 # adicionar novos funcionários
 def cadastrar_funcionario():
+    ultimo_id = carregar_ultimo_id()
     funcionarios = Funcionario.cadastrar_funcionario()
     with open('./arquivos/empregados.txt', 'a', encoding='utf-8') as arquivo:
         for funcionario in funcionarios:
             arquivo.write(str(funcionario))
+            ultimo_id = funcionario._id
+    salvar_id(ultimo_id)
+    return ultimo_id
 
 # TODO: apagar funcionários
 # TODO: imprimir só nome do funcionário
