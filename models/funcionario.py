a50938ddc44ff0e9aae1f0d495754d17a04858de
PedroAugusto2305 <p.augusto_23@outlook.com>
2024-04-16 10:47:07 -0300

    Gerador de IDs

--- models/funcionario.py
+++ models/funcionario.py
@@ -1,13 +1,15 @@
 class Funcionario:
 
-    def __init__(self, id, nome, cargo, salario):
-        self._id = id
+    ultimo_id = 0
+
+    def __init__(self, nome, cargo, salario):
+        self._id = Funcionario.gerar_id()
         self._nome = nome
         self._cargo = cargo
         self._salario = salario
 
     def __str__(self):
-        return f'{self._nome} | {self._cargo} | R$ {self._salario:.2f}\n'
+        return f'{self._id} | {self._nome} | {self._cargo} | R$ {self._salario:.2f}\n'
 
     @staticmethod
     def cadastrar_funcionario():
@@ -20,17 +22,17 @@ class Funcionario:
             if resposta == 'n':
                 break
             elif resposta == 's':
-                # id += 1
                 nome = input('Informe o nome do funcionário: ')
                 cargo = input('Informe o cargo do funcionário: ')
                 salario = float(input('Informe o salário do funcionário: '))
-                funcionarios.append(Funcionario(id, nome, cargo, salario))
+                funcionarios.append(Funcionario(nome, cargo, salario))
             else:
                 print(
                     'Resposta inválida. Por favor, digite "s" para sim ou "n" para não.')
         return funcionarios
-    
+
     # TODO: função que gera ids dos funcionários criados
-    @property
-    def gerar_id(id):
-        pass
+    @staticmethod
+    def gerar_id():
+        Funcionario.ultimo_id += 1
+        return Funcionario.ultimo_id
